1ab22357462acf7bb965116605801786e70f0a7b
Kirtan Modi <modikirtan19@gmail.com>
2024-05-11 17:25:50 -0500

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## Features
 
-It will copy all open files into a single file.
+It will copy all open files into a single file in the root of your project and also copy the content on your clipboard
 
+
+## Run 
+open the command pallete and type copy open files to single file
--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,6 @@
-// // The module 'vscode' contains the VS Code extensibility API
-// // Import the module and reference it with the alias vscode in your code below
-// import * as vscode from 'vscode';
-
-// // This method is called when your extension is activated
-// // Your extension is activated the very first time the command is executed
-// export function activate(context: vscode.ExtensionContext) {
-
-// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-// 	// This line of code will only be executed once when your extension is activated
-// 	console.log('Congratulations, your extension "copyopenfiles" is now active!');
-
-// 	// The command has been defined in the package.json file
-// 	// Now provide the implementation of the command with registerCommand
-// 	// The commandId parameter must match the command field in package.json
-// 	let disposable = vscode.commands.registerCommand('copyopenfiles.helloWorld', () => {
-// 		// The code you place here will be executed every time your command is executed
-// 		// Display a message box to the user
-// 		vscode.window.showInformationMessage('Hello World from CopyOpenFiles!');
-// 	});
-
-// 	context.subscriptions.push(disposable);
-// }
-
-// // This method is called when your extension is deactivated
-// export function deactivate() {}
-
-
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('copyOpenFiles.copyContents', () => {
@@ -38,12 +11,19 @@ export function activate(context: vscode.ExtensionContext) {
 			outputContent += `// ${doc.fileName}\n${doc.getText()}\n\n`;
 		});
 
-		fs.writeFile(vscode.workspace.rootPath + '/response.txt', outputContent, err => {
+		const folderPath = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0].uri.fsPath : '';
+		const filePath = path.join(folderPath, 'response.txt');
+
+		fs.writeFile(filePath, outputContent, err => {
 			if (err) {
 				vscode.window.showErrorMessage('Error writing file: ' + err.message);
 				return;
 			}
-			vscode.window.showInformationMessage('Files successfully written to response.txt');
+			vscode.env.clipboard.writeText(outputContent).then(() => {
+				vscode.window.showInformationMessage('Files successfully written to response.txt and copied to clipboard');
+			}, (clipboardError) => {
+				vscode.window.showErrorMessage('Error copying content to clipboard: ' + clipboardError.message);
+			});
 		});
 	});
 
